8aa07b948f8f451dadd34e56dce04865a97a7aca
elvis <elvisrozarioefr@gmail.com>
2021-08-03 14:15:54 -0400

    log merge

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,32 @@
 "use strict";
 
+const _ = require("lodash");
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    // sort the logs in the logSources
+    logSources.sort((a, b) => new Date(a.last.date).getTime() - new Date(b.last.date).getTime())
+
+    while (logSources.length > 0) {
+      const logEntry = logSources[0].last;
+      printer.print(logEntry)
+
+      logSources = await binaryInsert(logSources).catch((err) => { console.error(err); });
+    }
+
+    printer.done()
     resolve(console.log("Async sort complete."));
   });
 };
+
+async function binaryInsert(sortedLogSources) {
+  const usedLogSource = sortedLogSources.shift();
+  if (usedLogSource.drained) return sortedLogSources
+
+  await usedLogSource.pop();
+  // binary search and insert
+  sortedLogSources.splice(_.sortedIndexBy(sortedLogSources, usedLogSource, "last.date"), 0, usedLogSource);
+
+  return sortedLogSources;
+}
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,71 @@
 "use strict";
 
+const _ = require("lodash");
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  // sort the logs in the logSources
+  logSources.sort((a, b) => new Date(a.last.date).getTime() - new Date(b.last.date).getTime())
+
+  while (logSources.length > 0) {
+    const logEntry = logSources[0].last;
+    printer.print(logEntry)
+
+    logSources = binaryInsert(logSources)
+  }
+
+  printer.done()
   return console.log("Sync sort complete.");
+
 };
+
+function jsSort(logSources) {
+  if (logSources[0].drained) {
+    logSources.shift()
+    return logSources
+  }
+  logSources[0].pop()
+  // default JS sort
+  logSources.sort((a, b) => new Date(a.last.date).getTime() - new Date(b.last.date).getTime())
+  return logSources;
+}
+
+function binaryInsert(sortedLogSources) {
+  const usedLogSource = sortedLogSources.shift();
+  if (usedLogSource.drained) return sortedLogSources
+
+  usedLogSource.pop();
+  // binary search and insert
+  sortedLogSources.splice(_.sortedIndexBy(sortedLogSources, usedLogSource, "last.date"), 0, usedLogSource);
+
+  return sortedLogSources;
+}
+
+function loopSort(sortedLogSources) {
+  const usedLogSource = sortedLogSources.shift();
+  if (usedLogSource.drained) return sortedLogSources
+  usedLogSource.pop();
+  sortedLogSources.push(usedLogSource);
+
+  // loop from last to first moving each bigger element to the right
+  let i = sortedLogSources.length - 1;
+  const item = sortedLogSources[i];
+  while (i > 0 && item.last.date < sortedLogSources[i - 1].last.date) {
+    sortedLogSources[i] = sortedLogSources[i - 1];
+    i -= 1;
+  }
+  sortedLogSources[i] = item;
+  return sortedLogSources;
+}
+
+function checkSorted(logSources) {
+  let isSorted = true;
+  logSources.some((source, i) => {
+    if (logSources[i + 1] && source.last.date > logSources[i + 1].last.date) {
+      console.log(`Broke on ${i} since ${source.last.date} > ${logSources[i + 1].last.date}`)
+      isSorted = false;
+      return true
+    }
+  });
+  return isSorted
+}
